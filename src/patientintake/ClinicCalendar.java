e72b70ec6ff20cce472e369c369c9fae1a623d65
weaverj <weaver.je@gmail.com>
2018-08-17 21:35:36 -0500

    module 1 work

--- src/patientintake/ClinicCalendar.java
+++ src/patientintake/ClinicCalendar.java
@@ -1,12 +1,8 @@
 package patientintake;
 
-import javax.print.Doc;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ClinicCalendar {
 
@@ -17,18 +13,19 @@ public class ClinicCalendar {
    }
 
    public void addAppointment(String patientFirstName, String patientLastName, String doctorKey, String dateTime) {
-//      if (!Arrays.asList(Doctor.ncontains(doctorKey)) {
-//         throw new RuntimeException("Unrecognized identifier for doctor: [" + doctorKey + "]");
-//      }
-      Doctor doc = Doctor.valueOf(doctorKey);
+      Doctor doc = Doctor.valueOf(doctorKey.toLowerCase());
       LocalDateTime localDateTime;
       try {
-         localDateTime = LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("M/d/yyyy h:m a"));
+         localDateTime = LocalDateTime.parse(dateTime.toUpperCase(), DateTimeFormatter.ofPattern("M/d/yyyy h:mm a", Locale.US));
       } catch (Throwable t) {
-         throw new RuntimeException("Unable to create date time from: [" + dateTime + "], please enter with format [M/d/yyyy h:m a");
+         throw new RuntimeException("Unable to create date time from: [" + dateTime.toUpperCase() + "], please enter with format [M/d/yyyy h:mm a]");
       }
       PatientAppointment appointment = new PatientAppointment(patientFirstName, patientLastName, localDateTime, doc);
       appointments.add(appointment);
+   }
+
+   public List<PatientAppointment> getAppointments() {
+      return this.appointments;
    }
 
 }
--- src/patientintake/ClinicMain.java
+++ src/patientintake/ClinicMain.java
@@ -1,9 +1,71 @@
 package patientintake;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class ClinicMain {
 
-   public static void main(String[] args) {
-      System.out.println("Welcome to the Patient Intake Computer System!\n\n");
+   private static ClinicCalendar calendar;
 
+   public static void main(String[] args) throws Throwable {
+      calendar = new ClinicCalendar();
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Welcome to the Patient Intake Computer System!\n\n");
+      String lastOption = "";
+      while (!lastOption.equalsIgnoreCase("x")) {
+         lastOption = displayMenu(scanner);
+      }
+      System.out.println("\nExiting System...\n");
    }
+
+   private static String displayMenu(Scanner scanner) throws Throwable {
+      System.out.println("Please select an option:");
+      System.out.println("1. Enter a Patient Appointment");
+      System.out.println("2. View All Appointments");
+      System.out.println("X.  Exit System.");
+      System.out.print("Option: ");
+      String option = scanner.next();
+      switch (option) {
+         case "1": performPatientEntry(scanner);
+                 return option;
+         case "2": performAllAppointments();
+                 return option;
+         default: System.out.println("Invalid option, please re-enter.");
+                  return option;
+      }
+   }
+
+   private static void performPatientEntry(Scanner scanner) {
+      scanner.nextLine();
+      System.out.println("\n\nPlease Enter Appointment Info:");
+      System.out.print("  Patient Last Name: ");
+      String lastName = scanner.nextLine();
+      System.out.print("  Patient First Name: ");
+      String firstName = scanner.nextLine();
+      System.out.print("  Appointment Date (M/d/yyyy h:m a): ");
+      String when = scanner.nextLine();
+      System.out.print("  Doctor Last Name: ");
+      String doc = scanner.nextLine();
+      try {
+         calendar.addAppointment(firstName, lastName, doc, when);
+      } catch (Throwable t) {
+         System.out.println("Error!  " + t.getMessage());
+         return;
+      }
+      System.out.println("Patient entered successfully.\n\n");
+   }
+
+   private static void performAllAppointments() throws Throwable {
+      System.out.println("\n\nAll Appointments in System:");
+      for (PatientAppointment appointment : calendar.getAppointments()) {
+         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a");
+         String apptTime = formatter.format(appointment.getAppointmentDateTime());
+         System.out.println(String.format("%s:  %s, %s\t\tDoctor: %s", apptTime, appointment.getPatientLastName(),
+            appointment.getPatientFirstName(), appointment.getDoctor().getName()));
+      }
+      System.out.println("\nPress any key to continue...");
+      System.in.read();
+      System.out.println("\n\n");
+   }
+
 }
