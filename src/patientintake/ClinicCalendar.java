c5031c64b255cf465efe72ff841f058d135aaebf
weaverj <weaver.je@gmail.com>
2018-08-23 19:21:35 -0500

    module 1 work

--- src/patientintake/ClinicCalendar.java
+++ src/patientintake/ClinicCalendar.java
@@ -12,15 +12,19 @@ public class ClinicCalendar {
       this.appointments = new ArrayList<>();
    }
 
-   public void addAppointment(String patientFirstName, String patientLastName, String doctorKey, String dateTime) {
+   public void addAppointment(String patientFirstName, String patientLastName, String doctorKey,
+                              String dateTime) {
       Doctor doc = Doctor.valueOf(doctorKey.toLowerCase());
       LocalDateTime localDateTime;
       try {
-         localDateTime = LocalDateTime.parse(dateTime.toUpperCase(), DateTimeFormatter.ofPattern("M/d/yyyy h:mm a", Locale.US));
+         localDateTime = LocalDateTime.parse(dateTime.toUpperCase(),
+            DateTimeFormatter.ofPattern("M/d/yyyy h:mm a", Locale.US));
       } catch (Throwable t) {
-         throw new RuntimeException("Unable to create date time from: [" + dateTime.toUpperCase() + "], please enter with format [M/d/yyyy h:mm a]");
+         throw new RuntimeException("Unable to create date time from: [" +
+            dateTime.toUpperCase() + "], please enter with format [M/d/yyyy h:mm a]" + t.getMessage());
       }
-      PatientAppointment appointment = new PatientAppointment(patientFirstName, patientLastName, localDateTime, doc);
+      PatientAppointment appointment = new PatientAppointment(patientFirstName, patientLastName,
+         localDateTime, doc);
       appointments.add(appointment);
    }
 
